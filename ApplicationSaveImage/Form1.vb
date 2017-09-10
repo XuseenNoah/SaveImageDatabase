0dcf7add0f6a2652107bf704f0ee6eb19417facb
xuseennoah <lect2017@gmail.com>
2017-09-10 01:01:19 -0700

    FirstCommit

---
+++ ApplicationSaveImage/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Dim fopen As New OpenFileDialog
+            fopen.FileName = ""
+            fopen.Filter = "Image Files (*.jpg)|*.jpg|(*.jpeg)|*.JPEG|(*.gif)|*.gif|(*.png)|*.png|All Files (*.*)|*.*"
+            fopen.ShowDialog()
+            PictureBox1.Image = System.Drawing.Bitmap.FromFile(fopen.FileName)
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        PictureBox1.Image = System.Drawing.Bitmap.FromFile(Application.StartupPath & "\NoImage\" & "noimage.gif")
+
+    End Sub
+End Class
